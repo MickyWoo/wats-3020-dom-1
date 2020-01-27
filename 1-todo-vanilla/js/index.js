ca89135915bde3c22704c7d937faf2e0a6b7f010
Micky Woo <mwoo@seattleu.edu>
2020-01-27 14:05:54 -0800

    pt1

--- 1-todo-vanilla/js/index.js
+++ 1-todo-vanilla/js/index.js
@@ -1,33 +1,33 @@
 //TODO add event to listen to when DOM is ready to access
-document.addEventListener("DOMContentLoaded", (event) => {  // part of mdn search
+document.addEventListener("DOMContentLoaded", (event) => { // part of mdn search
   document.addEventListener("submit", (event) => {
-    event.preventDefault()  // helps against refreshing page
-  }); {
+    event.preventDefault() // helps against refreshing page
+
     //TODO add event listener to signal that user is submitting form
-    {
-      // TODO add code to keep form default submit behavior from occurring
 
-      // TODO add a selector that selects the input element with the attribute name equal to "item-input"
-      //  not a class or id
-      // from advanced version of querySelector set up is with <input type="text"  name="item-input"/>
-      // from the index.html you just take the input portion and cut out "type-"text" and everything in quotations
-      let itemInput = document.querySelector("input[name = 'item-input']");
+    // TODO add code to keep form default submit behavior from occurring
 
-
-      let itemValue = itemInput.value;
-      //test to be sure that user entered a value
-      if (itemValue.length !== 0) {
-        //create new item
+    // TODO add a selector that selects the input element with the attribute name equal to "item-input"
+    //  not a class or id
+    // from advanced version of querySelector set up is with <input type="text"  name="item-input"/>
+    // from the index.html you just take the input portion and cut out "type-"text" and everything in quotations
+    let itemInput = document.querySelector("input[name = 'item-input']");
 
 
-        //TODO create a list item with a checkbox and a label containing the user input string
-        let newItemEl = document.createElement("li");
+    let itemValue = itemInput.value;
+    //test to be sure that user entered a value
+    if (itemValue.length !== 0) {
+      //create new item
+
+
+      //TODO create a list item with a checkbox and a label containing the user input string
+      let newItemEl = document.createElement("li");
       let newLabel = document.createElement("label");
 
 
       //create checkbox element
       // then 
-      let newCheckBox =  document.createElement("input");
+      let newCheckBox = document.createElement("input");
       // setAttribute from reading ( wouldnt know of top of my head tho)
       newCheckBox.setAttribute("type", "checkbox")
       // appendChild is Specifically from reading it adds the input values back to index.html
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", (event) => {  // part of mdn searc
       //get a reference to list and append list item
       // we chose todo-list because its the list created 
       // .todo-list is a class hence the .
-      let list = document.querySelector('.todo-list'); 
+      let list = document.querySelector('.todo-list');
 
       list.appendChild(newItemEl);
       //clear the user input
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", (event) => {  // part of mdn searc
       //TODO add click event toggle that strikes out text in label associated with list item
       newCheckBox.addEventListener("click", function (event) {
         // TODO check if the textDecoration style on this.nextSibling is "line-through"
-        if (this.nextSibling.style.textDecoration == "line-through" ) {
+        if (this.nextSibling.style.textDecoration == "line-through") {
           this.nextSibling.style.textDecoration = "";
           // TODO set the textDecoration style on this.nextSibling to blank (empty string)
         } else {
